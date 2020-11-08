7738df3070385605c65ed79219b97f04f8e93793
Jedrzej <42713788+Extollite@users.noreply.github.com>
2020-11-08 14:30:32 +0100

    Update for newest transferpe package.

--- Sources/TransferApp/main.swift
+++ Sources/TransferApp/main.swift
@@ -1,10 +1,21 @@
 import Foundation
 import TransferPe
+import NIO
+import RakNetSwift
 
 let arguments: [String] = Array(CommandLine.arguments.dropFirst())
 
 var ip = "mcpe.to"
 var port = 19132
+var group = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
+
+defer {
+    do {
+        try group.syncShutdownGracefully()
+    } catch {
+        print(error)
+    }
+}
 
 if(arguments.count == 1){
     ip = arguments[0]
@@ -13,14 +24,16 @@ if(arguments.count == 1){
     port = Int(arguments[1])!
 }
 
-let server = TransferPe(ip, port)
-let closeFuture = server.run()
+
+let server = TransferPe(ip, port, false, withHandler: {
+    print("Server turned off!")
+})
 
 signal(SIGINT) { s in server.close() }
 
-do {
-    try closeFuture!.wait()
-} catch {
-    print(error)
+server.run(group)
+
+while(server.isRunning) {
+    //NOOP
 }
 
